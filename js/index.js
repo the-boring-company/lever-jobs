b0b97ec6ef8673d51a4b2872b2e08b1cb498a050
Shreehar <joshishreehar@gmail.com>
2024-09-04 20:46:19 -0700

    add repo

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$( "button" ).on( "click", function() {
+
+  var inputAccountName = $('#account').val();
+
+  var postingsCode = "&lt;div id='lever-jobs-container'&gt;&lt;/div&gt;&lt;script type='text/javascript'&gt;window.leverJobsOptions = {accountName: '" + inputAccountName +"', includeCss: true};&lt;/script&gt;&lt;script type='text/javascript' src='https://ryangarza117.github.io/lever-departments/index.js'&gt;&lt;/script&gt;";
+
+  $(".success, .error").remove();
+  if (inputAccountName.length) {
+    $("#code").append('<p class="success">'+postingsCode+'</p>');
+  } else {
+    $("#code").append('<p class="error">You need to enter your account name</p>');
+  }
+
+});
